0e3f017014203e7142e84b5ac1d334a98ab19db7
Arshlankhann <arshlank894@gmail.com>
2025-02-15 11:39:08 +0530

    made some changes

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
         <div className="about__data grid">
           <div className="about__info">
             <p className="about__description">
-            I am Arshlan Khan, a passionate web developer from Lucknow, India, with rich experience in website design, development, and customization. Over the years, I’ve had the opportunity to work on a diverse range of projects, from small-scale web applications to large enterprise systems, honing my skills in building scalable, user-friendly, and high-performance applications. For more information, feel free to visit my website: <a href='https://arshlankhan.com/'>Ashlankhan.com</a> .
+            I am Arshlan Khan, a passionate web developer from Lucknow, India, with rich experience in website design, development, and customization. Over the years, I’ve had the opportunity to work on a diverse range of projects, from small-scale web applications to large enterprise systems, honing my skills in building scalable, user-friendly, and high-performance applications. For more information, feel free to visit my website: <a href='https://arshlankhan.com/'>https://arshlankhan.com/</a> .
 
             </p>
-            <a href="https://drive.google.com/file/d/1P0u6NACu2XYKjhbX0nFcgf5Uend9oAYu/view?usp=sharing" className="btn">Download CV</a>
+            <a href="https://drive.google.com/file/d/1OJxj7J03JTxWKOKHCQWFOXKQMGNJTrQb/view?usp=drive_link" className="btn">Download CV</a>
           </div>
           <div className="about__skills grid">
             <div className="skills__data">
